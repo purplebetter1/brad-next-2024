84c2123955f842564111ee30e51aa78a5402b0c1
andrew <booleanorama@gmail.com>
2024-08-24 14:07:04 -0400

    add sign in button

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth/next";
 
-const handler = NexAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,4 +1,6 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
   providers: [
@@ -18,15 +20,30 @@ export const authOptions = {
     // Invoked on successful sign in
     async signIn({ profile }) {
       // Connect to db
+      await connectDB();
       // Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // if not create user
+      if (!userExists) {
+        // truncate user name if too long
+        const username = profile.name.slice(0, 20);
+        await User.create({
+          email: profile.email,
+          username: username,
+          image: profile.picture,
+        });
+      }
       // return true to allow sign in
+      return true;
     },
     // Session callback function that modifies the session object
     async session({ session }) {
       // Get user from db
+      const user = await User.findOne({ email: session.user.email });
       // Assign user id from session
+      session.user.id = user._id.toString;
       // return session
+      return session;
     },
   },
 };
