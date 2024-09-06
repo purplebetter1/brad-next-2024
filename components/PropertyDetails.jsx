73b85f93b040ad5412adea70fa916fc2e31a709d
andrew <booleanorama@gmail.com>
2024-09-06 11:17:51 -0400

    add google geocode config

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,3 +1,5 @@
+import PropertyMap from "@/components/PropertyMap";
+
 import {
   FaTimes,
   FaBed,
@@ -91,7 +93,7 @@ const PropertyDetails = ({ property }) => {
         </ul>
       </div>
       <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-        <div id="map"></div>
+        <PropertyMap property={property} />
       </div>
     </main>
   );
