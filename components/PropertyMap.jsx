1e630a7b22f4867c5536cfcaf72142077496b41a
andrew <booleanorama@gmail.com>
2024-09-08 11:36:15 -0400

    add map

--- components/PropertyMap.jsx
+++ components/PropertyMap.jsx
@@ -1,6 +1,11 @@
 "use client";
 import { setDefaults, fromAddress } from "react-geocode";
 import { useEffect, useState } from "react";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Image from "next/image";
+import pin from "@/assets/images/pin.svg";
+import Spinner from "@/components/Spinner";
 
 const PropertyMap = ({ property }) => {
   const [lat, setLat] = useState(null);
@@ -51,12 +56,30 @@ const PropertyMap = ({ property }) => {
     fetchCords();
   }, []);
 
-  if (loading) return <h3>Loading...</h3>;
+  if (loading) return <Spinner />;
 
   if (geocodeError)
     return <div className="text-xl">No location data found</div>;
 
-  return <div>Map</div>;
+  return (
+    !loading && (
+      <Map
+        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        mapLib={import("mapbox-gl")}
+        initialViewState={{
+          longitude: lng,
+          latitude: lat,
+          zoom: 15,
+        }}
+        style={{ width: "100%", height: 500 }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      >
+        <Marker longitude={lng} latitude={lat} anchor="bottom">
+          <Image src={pin} alt="location" width={40} height={40} />
+        </Marker>
+      </Map>
+    )
+  );
 };
 
 export default PropertyMap;
