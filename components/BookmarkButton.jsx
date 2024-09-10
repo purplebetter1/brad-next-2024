ed27d6a9bcd01a0c9710b213db083d3df662b77b
andrew <booleanorama@gmail.com>
2024-09-10 12:02:13 -0400

    add check bookmarked status

--- components/BookmarkButton.jsx
+++ components/BookmarkButton.jsx
@@ -1,6 +1,8 @@
 "use client";
 
+import { useState, useEffect } from "react";
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import { FaBookmark } from "react-icons/fa";
 import { toast } from "react-toastify";
 import { useSession } from "next-auth/react";
@@ -8,6 +10,21 @@ import { useSession } from "next-auth/react";
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
   const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+    checkBookmarkStatus(property._id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
 
   const handleClick = async () => {
     if (!userId) {
@@ -17,11 +34,23 @@ const BookmarkButton = ({ property }) => {
 
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
 
-  return (
+  if (loading) {
+    return <p className="text-center">Loading...</p>;
+  }
+
+  return isBookmarked ? (
+    <button
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+      onClick={handleClick}
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
     <button
       className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
       onClick={handleClick}
