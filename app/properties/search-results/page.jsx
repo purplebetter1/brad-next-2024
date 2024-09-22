e2a5cac3a1bbb287d465bb63a8263c63b2f486fe
andrew <booleanorama@gmail.com>
2024-09-22 10:24:13 -0400

    display search results

--- app/properties/search-results/page.jsx
+++ app/properties/search-results/page.jsx
@@ -1,6 +1,10 @@
+import Link from "next/link";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 import { convertToSerializeableObject } from "@/utils/convertToObject";
+import PropertyCard from "@/components/PropertyCard";
+import PropertySearchForm from "@/components/PropertySearchForm";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
 
 const SearchResultsPage = async ({
   searchParams: { location, propertyType },
@@ -29,7 +33,36 @@ const SearchResultsPage = async ({
   const properties = convertToSerializeableObject(propertiesQueryResults);
   console.log(properties);
 
-  return <div>Search Results</div>;
+  return (
+    <>
+      <section className="bg-blue-700 py-4">
+        <div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+          <PropertySearchForm />
+        </div>
+      </section>
+      <section className="px-4 py-6">
+        <div className="container-xl lg:container m-auto px-4 py-6">
+          <Link
+            href="/properties"
+            className="flex items-center 
+          text-blue-500 hover:underline mb-3"
+          >
+            <FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Properties
+          </Link>
+          <h1 className="text-2xl mb-4">Search Results</h1>
+          {properties.length === 0 ? (
+            <p>No Search Results</p>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              {properties.map((property) => (
+                <PropertyCard key={property._id} property={property} />
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SearchResultsPage;
